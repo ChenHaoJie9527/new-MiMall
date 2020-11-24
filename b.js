35ffe23916709737de4d51f866b4f1c4eb1e2c24
chenhaojie <a18520145493@163.com>
2020-11-24 18:00:41 +0800

    add

--- b.js
+++ b.js
@@ -14,3 +14,9 @@ then((results) => console.log(results));
 const num = 10;
 console.log(num.toFixed(2));
 console.log(num.toPrecision(4));
+
+let numObj = 5.123456;
+console.log(numObj.toPrecision())    // logs '5.123456'
+console.log(numObj.toPrecision(3))   // logs '5.1235'
+console.log(numObj.toPrecision(2))   // logs '5.1'
+console.log(numObj.toPrecision(1))   // logs '5'
