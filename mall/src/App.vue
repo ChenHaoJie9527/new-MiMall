2e5d074acabd69c16f1987044c6d71c583e3f185
chenhaojie <a18520145493@163.com>
2020-11-26 09:51:15 +0800

    CORS跨域-JSONP方案

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -7,15 +7,21 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from "axios";
+// import axios from "axios";
+import jsonp from "jsonp";
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
   async created() {
-    const res = await axios.get("https://www.easy-mock.com/mock/5fbe19b90295b12de9e9a472/example/mock");
-    console.log(res)
+    // const res = await axios.get("https://www.easy-mock.com/mock/5fbe19b90295b12de9e9a472/example/mock");
+    // console.log(res)
+    // jsonp在技术上是一个函数，但在跨域通信方面是一段脚本JS，并不是一个请求，因此往往是通过回调函数中的参数进行传输数据
+    const url = "https://www.imooc.com/search/hotwords";
+    jsonp(url,(error,data)=>{
+      console.log(data);
+    })
   },
 }
 </script>
