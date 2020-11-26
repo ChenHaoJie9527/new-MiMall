3218f148ee6b65b170b029e3d722ab10f9c92985
chenhaojie <a18520145493@163.com>
2020-11-26 11:04:39 +0800

    rm helloword

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 // import axios from "axios";
 import jsonp from "jsonp";
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   async created() {
     // const res = await axios.get("https://www.easy-mock.com/mock/5fbe19b90295b12de9e9a472/example/mock");
