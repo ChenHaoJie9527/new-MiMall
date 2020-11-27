2535277fe767e88b3f482bfbc252801b165438f5
chenhaojie <a18520145493@163.com>
2020-11-27 17:39:52 +0800

    响应拦截器和设置默认接口代理及时间

--- mall/src/main.js
+++ mall/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue';
+import axios from "axios";
+import VueAxios from "vue-axios";
 import App from './App.vue';
 import router from "./router/index";
 Vue.config.productionTip = false;
 
+axios.defaults.baseURL = "/api";
+axios.defaults.timeout = 8000;
+
+axios.interceptors.response.use((response) => {
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href = "/#/login";
+  }else{
+    alert(res.msg);
+  }
+})
+
+
+
+
+
+
+Vue.use(VueAxios, axios);
 new Vue({
   router,
   render: h => h(App),
