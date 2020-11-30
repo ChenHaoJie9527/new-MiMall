ef5a5f00a92077d7077f93e800c7dc88b9515c8c
chenhaojie <a18520145493@163.com>
2020-11-30 17:56:01 +0800

    mock本地json

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -5,22 +5,14 @@
 </template>
 
 <script>
-// import axios from "axios";
-// import jsonp from "jsonp";
-// import { storage } from "./storage/index";
 export default {
   name: "App",
   components: {},
   async created() {
-    // const res = await axios.get("https://www.easy-mock.com/mock/5fbe19b90295b12de9e9a472/example/mock");
-    // console.log(res)
-    // jsonp在技术上是一个函数，但在跨域通信方面是一段脚本JS，并不是一个请求，因此往往是通过回调函数中的参数进行传输数据
-    // const url = "/api/search/hotwords";
-    // jsonp(url,(error,data)=>{
-    //   console.log(data);
-    // })
-    // console.log(storage.getItem("user"));
-    // console.log(storage.clear("user","age"));
+    // 1.本地加载请求静态json数据
+    this.axios.get("/mock/user/login.json").then(res=>{
+      console.log(res);
+    })
   },
 };
 </script>
