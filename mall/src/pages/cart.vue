2f498776915bed196e944a5452f2ce765df533d6
ChenHaoJie9527 <18312883857@chenhao_jie.com>
2020-12-12 21:17:43 +0800

    自定义指令和debounce防抖函数使用

--- mall/src/pages/cart.vue
+++ mall/src/pages/cart.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <order-header></order-header>
-      cart
+      <input type="text" v-model="inputValue" v-layz:input="inputValue" @input="handClickInput">
     <order-footer></order-footer>
   </div>
 </template>
@@ -9,11 +9,30 @@
 <script>
 import OrderHeader from "../components/OrderFooter";
 import OrderFooter from "../components/OrderFooter";
+import {debounceAjax} from "../utils/debounce";
 export default {
     name: "cart",
     components: {
     OrderHeader,
     OrderFooter,
+  },
+  data() {
+    return {
+      inputValue: ""
+    }
+  },
+  directives:{
+    layz: {
+      update: function(binding){
+        // console.log(binding.value)
+        debounceAjax(binding.value)
+      }
+    }
+  },
+  methods: {
+    handClickInput(){
+      
+    },
   },
 }
 </script>
