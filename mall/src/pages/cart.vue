407bc51d95a0106ab6772efd093ab4a62fdac6be
ChenHaoJie9527 <18312883857@chenhao_jie.com>
2020-12-13 22:03:07 +0800

    添加注释

--- mall/src/pages/cart.vue
+++ mall/src/pages/cart.vue
@@ -11,7 +11,7 @@
       />
     </div>
     <ul v-show="isshow">
-      <li v-for="(item, index) of list" :key="index">
+      <li v-for="(item, index) of staff" :key="index">
         <p class="name">{{ item.name }}</p>
         <p class="age">{{ item.age }}</p>
         <p class="job">{{ item.job }}</p>
@@ -39,7 +39,7 @@ import {
   SearchKeyValue,
   SearchMultiValue,
   SearchSingleAndMultiValue,
-  SearchMultiKeyAndMultiValue
+  SearchMultiKeyAndMultiValue,
 } from "../utils/searchKeyValue";
 export default {
   name: "cart",
@@ -52,7 +52,7 @@ export default {
       inputValue: "",
       flag: true,
       inputMove: "",
-      list: [
+      staff: [
         { name: "April", job: "programmer", age: "18", hobby: "study" },
         { name: "Shawn", job: "student", age: "8", hobby: "study" },
         { name: "Leo", job: "teacher", age: "28", hobby: "play" },
@@ -75,26 +75,29 @@ export default {
     },
   },
   created() {
-    //单条件精准查询
-    const res = SearchKeyValue(this.list, "job", "cook");
+    //单条件精准查询 // 函数执行 在 “staff” 对象中查找 “job” 值为 “programmer” 的数据
+    const res = SearchKeyValue(this.staff, "job", "cook");
     console.log(res);
     const filters = {
       name: "Leo",
       age: "19",
     };
-    // 多条件精准查询
-    const res1 = SearchMultiValue(this.list, filters);
+    // 多条件精准查询 函数执行 在 “staff” 对象中查找 “name” 值为 “April” 和 “hobby” 值为 “study” 的数据
+    const res1 = SearchMultiValue(this.staff, filters);
     console.log(res1);
-    //单条件多值精准查找
-    const res2 = SearchSingleAndMultiValue(this.list, "name", ["Leo", "Shawn"]);
+    //单条件多值精准查找 // 函数执行 在 “staff” 对象中查找 “job” 值为 “programmer” 和 “student” 的数据
+    const res2 = SearchSingleAndMultiValue(this.staff, "name", [
+      "programmer",
+      "student",
+    ]);
     console.log(res2);
-
+    // 函数执行 在 “staff” 对象中查找 “age” 值为 “8”或“18” 和 “hobby” 值为 “play”或“sleep” 的数据
     const filters1 = {
       age: [8, 18],
       hobby: ["play", "sleep"],
-    }
-    const res3 = SearchMultiKeyAndMultiValue(this.list,filters1);
-    console.log(res3)
+    };
+    const res3 = SearchMultiKeyAndMultiValue(this.staff, filters1);
+    console.log(res3);
   },
   methods: {
     previewIsShow() {
